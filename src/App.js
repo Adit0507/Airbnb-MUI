d67e1d77077faef6068e8debac5cf73718a94b7e
Adit0507 <adi05072002@gmail.com>
2022-12-18 13:51:43 +0530

    Finished :rocket:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import Box from '@mui/material/Box';
 import Header from 'components/Header';
 import OptionsTab from 'components/OptionsTab';
+import LocationCards from 'components/LocationCards';
+import Footer from 'components/Footer';
+import FooterMenu from 'components/FooterMenu';
+import MobileFooter from 'components/MobileFooter';
+
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+
+import { displayOnDesktop } from 'themes/commonStyles';
 
 function App() {
   return (
-      <Box>
-        <Header />
-        <OptionsTab />
+    <>
+      <CssBaseline />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100vh'
+        }}
+      >
+        <Box>
+          <Header />
+          <OptionsTab />
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            flexGrow: 1,
+            height: 100,
+            overflowY: 'scroll'
+          }}
+        >
+          <Container maxWidth="xl" sx={{ mb: 3 }}>
+            <LocationCards />
+            <Box sx={{
+              display: { xs: 'flex', md: 'none' },
+            }}
+            >
+              <MobileFooter />
+            </Box>
+          </Container>
+        </Box>
+        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <FooterMenu />
+        </Box>
+        <Box sx={displayOnDesktop}>
+          <Footer />
+        </Box>
       </Box>
-    );
+    </>
+  );
 }
 
-export default App;
+export default App; 
